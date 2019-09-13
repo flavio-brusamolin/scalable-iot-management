715673918effd802dd5ae04415a626db5f5e4096
“FlavioBrusamolin” <flaviobrusamolin@gec.inatel.br>
2019-09-13 13:01:19 -0300

    Modified server error interceptor

--- iot-management-app/src/app/core/interceptors/server-error.interceptor.ts
+++ iot-management-app/src/app/core/interceptors/server-error.interceptor.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpRequest, HttpHandler, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+declare const $: any;
 
 @Injectable()
 export class ServerErrorInterceptor implements HttpInterceptor {
 
-    constructor() { }
+    constructor(private router: Router) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(
             retry(1),
             catchError((error: HttpErrorResponse) => {
                 if (error.error.message === 'Falha ao autenticar token') {
-                    window.location.href = '/login';
+                    $('.modal').modal('hide');
+                    this.router.navigate(['/login']);
                     localStorage.clear();
+                    return throwError('Sessão expirada. É necessário realizar login novamente');
                 } else {
                     return throwError(error);
                 }
