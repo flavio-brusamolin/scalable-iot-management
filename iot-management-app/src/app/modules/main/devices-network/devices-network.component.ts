413ccd8288215401c5c1f90ec6fb8c8c72dd665c
Leonardo Eufrazio <leonardoeufrazio@gec.inatel.br>
2019-09-24 22:43:33 -0300

    Added network topology

--- iot-management-app/src/app/modules/main/devices-network/devices-network.component.ts
+++ iot-management-app/src/app/modules/main/devices-network/devices-network.component.ts
@@ -1,29 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { DevicesNetworkService } from './devices-network.service';
 
-import { sigma } from 'sigma';
-
-const s = new sigma();
+import Sigma from 'sigma';
 
 @Component({
   selector: 'app-devices-network',
   templateUrl: './devices-network.component.html',
   styleUrls: ['./devices-network.component.css']
 })
-export class DevicesNetworkComponent implements OnInit {
+export class DevicesNetworkComponent implements OnInit, OnDestroy {
 
-  devices: any = [];
+  sigma: any;
+  intervalId: any;
 
   constructor(private networkService: DevicesNetworkService) { }
 
   ngOnInit() {
-    this.listDevices();
+    this.generateGraph();
+    this.draw();
+    this.intervalId = setInterval(() => this.draw(), 1000);
   }
 
-  async listDevices() {
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  generateGraph() {
+    this.sigma = new Sigma({
+      renderer: {
+        container: 'network-topology',
+        type: 'canvas'
+      },
+      settings: {
+        drawLabels: false,
+        // enableHovering: false
+      }
+    });
+  }
+
+  async draw() {
     const { devices } = await this.networkService.listDevices();
-    this.devices = devices;
+
+    const graph = {
+      nodes: [{ id: 'n0', label: 'Center element', x: 0, y: 0, size: 3, color: '#000000' }],
+      edges: []
+    };
+
+    for (let i = 0; i < devices.length; i++) {
+      const deviceNode = {
+        id: `n${i + 1}`,
+        label: devices[i].name,
+        x: Math.cos(Math.PI * 2 * i / devices.length),
+        y: Math.sin(Math.PI * 2 * i / devices.length),
+        size: 3,
+        color: 'black',
+        // type: 'square'
+      };
+
+      const deviceEdge = {
+        id: `e${i}`,
+        source: 'n0',
+        target: `n${i + 1}`,
+        color: devices[i].isConnected ? 'green' : 'red',
+        size: 1,
+        // type: 'curve'
+      };
+
+      graph.nodes.push(deviceNode);
+      graph.edges.push(deviceEdge);
+    }
+
+    this.sigma.graph.clear();
+    this.sigma.graph.read(graph);
+    this.sigma.refresh();
   }
 
 }
